3b1d1d8c9edf01e379c1297fd2a23077b95b465e
anuragjha <toanuragjha@gmail.com>
2021-07-30 01:44:40 -0700

    main: Summary: Listener accepts tcp conn stats a go routine to process it. Process reads from it until n sec timeout is hit coz of idle conn. it Created a Listener to loop blocking Accept. Description: When a connection comes along, blocking Accept returns with Conn. Conn is passed down through the abstractions until conn is used. conn is first derfer closed. Then loop over; a. conn readline (blocking with timeout), b. If readLine returns then read the content into a buffer and then c. write content from buffer to stdErr.

--- main.go
+++ main.go
@@ -2,10 +2,50 @@ package main
 
 import (
 	"log"
+	"net"
+	"os"
+	"time"
 )
 
 const version = "v0.0.0"
 
 func main() {
 	log.Print("Starting go-proxy:", version)
+
+	l, err := net.Listen("tcp", "localhost:7878")
+	if err != nil {
+		log.Fatal("Listener: ", err)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal("Conn: ", err)
+		}
+
+		go copyToStdErr(conn)
+	}
+}
+
+func copyToStdErr(conn net.Conn) {
+	// Using Golang library to copy in Writer from Reader
+	// n, err := io.Copy(os.Stderr, conn)
+	//log.Printf("Copied %d bytes; finished with err %v", n, err)
+
+	// Implemented custom version of copy where we set a conn idle timeout and also conn close
+	defer conn.Close()
+	for {
+		err := conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		if err != nil {
+			log.Printf("SetReadDeadline finished with err: %v", err)
+		}
+		var buf [128]byte
+		n, err := conn.Read(buf[:])
+		if err != nil {
+			log.Printf("Read from conn finished with err: %v", err)
+			return
+		}
+		os.Stderr.Write(buf[:n])
+	}
+
 }
